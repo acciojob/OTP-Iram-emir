95b7941a4c321ef1dd50262b0c792b5a0c7f3e0d
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2025-12-15 22:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+// focus first input by default
+inputs[0].focus();
+
+inputs.forEach((input, index) => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      input.value = "";
+
+      if (index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener("input", () => {
+    if (input.value !== "" && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+});
+
